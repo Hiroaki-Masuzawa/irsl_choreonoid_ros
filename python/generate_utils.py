e6001323f0ed0084160cddb652051ca88cce4942
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2026-01-16 14:48:00 +0900

    wip

---
+++ python/generate_utils.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+def get_jointlist(rbody):
+    num_joint = rbody.getNumJoints()
+    joints = [rbody.getJoint(idx) for idx in range(num_joint)]
+    return joints
+
+def get_jointnamelist(rbody):
+    joints = get_jointlist(rbody)
+    jointnames = [j.jointName for j in joints]
+    return jointnames
